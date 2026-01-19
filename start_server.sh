20a4c0ae2fa08520fb1f266c03af06a869898c07
Zhixiang <jorkin_china@hotmail.com>
2026-01-19 11:54:27 +0800

    fix: requirements

--- start_server.sh
+++ start_server.sh
@@ -7,4 +7,4 @@ echo "API Docs: http://localhost:8000/docs"
 echo ""
 
 cd "$(dirname "$0")"
-uvicorn app.main:app --reload --port 8000 --timeout-keep-alive 180
+uvicorn app.main:app --host 0.0.0.0 --reload --port 8000 --timeout-keep-alive 180
